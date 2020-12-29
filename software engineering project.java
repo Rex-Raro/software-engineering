f53ce5ae0870e9c1ad0b1ae850f3870d2ebe6e45
unknown <Rex Valoa@VALOAREX.bci.lan.ck>
2020-12-28 17:48:19 -1000

    Commit software engineering project file to github

---
+++ software engineering project.java
@@ -0,0 +1,10 @@
+// Java program to print name on the screen
+ 
+public class software{
+ 
+	// Program begins with a call to main().
+ 	public static void main(String args[])
+	{
+		System.out.println("Software Enginnering Project");
+	}
+}
